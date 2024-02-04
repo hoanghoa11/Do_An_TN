9dcd59b1159509a7a6854c21013aae0581087cd5
hoanghoa11 <101441919+hoanghoa11@users.noreply.github.com>
2024-02-04 15:30:47 +0700

    Add files via upload

---
+++ models/socialNetwork.js
@@ -0,0 +1,20 @@
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class SocialNetwork extends Model {
+    static associate(models) {
+    }
+  };
+  SocialNetwork.init({
+    name: DataTypes.STRING,
+    color: DataTypes.STRING,
+    icon: DataTypes.STRING,
+    link: DataTypes.STRING,
+    status: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'SocialNetwork',
+  });
+  return SocialNetwork;
+};
